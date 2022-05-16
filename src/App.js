db0d513701c2386eac550365420ee986fda658b8
yuxuanzhao <785315305@qq.com>
2022-05-16 22:07:15 +0100

    button

--- src/App.js
+++ src/App.js
@@ -1,10 +1,9 @@
 import Header from './components/Header'
 
 function App() {
-
   return (
     <div className="container">
-      <Header />
+      <Header title = 'Task Tracker'/>
     </div>
   );
 }
--- src/components/Header.js
+++ src/components/Header.js
@@ -1,11 +1,27 @@
-const Header = () => {
+import PropTypes from 'prop-types'
+import Button from './Button'
+
+const Header = ({ title }) => {
   return (
     <header>
         <h1>
-            Task Tracker
+            {title}
         </h1>
+        <Button color='green' text='Hello'/>
     </header>
   )
 }
+
+Header.defaultProps = {
+    title: 'Title not set yet',
+}
+
+Header.propTypes = {
+    title: PropTypes.string.isRequired,
+}
+
+
+// css in js, usually using it dynamically
+// const style = {color : 'red', backgroundColor : 'black'}
 
 export default Header
