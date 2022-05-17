ba30d3993d5fc735e4090102de9569e04545aa08
yuxuanzhao <785315305@qq.com>
2022-05-17 23:40:26 +0100

    add button toggle

--- src/components/Button.js
+++ src/components/Button.js
@@ -1,10 +1,6 @@
 import PropTypes from 'prop-types'
 
-const Button = ({ color, text }) => {
-
-    const onClick = () => {
-        console.log('click')
-    }
+const Button = ({ color, text, onClick }) => {
 
   return (
     <button onClick={onClick} style = {{backgroundColor : color}} className="btn">
--- src/components/Header.js
+++ src/components/Header.js
@@ -1,13 +1,11 @@
 import PropTypes from 'prop-types'
 import Button from './Button'
 
-const Header = ({ title }) => {
+const Header = ({ title, onAdd, showAdd }) => {
   return (
-    <header>
-        <h1>
-            {title}
-        </h1>
-        <Button color='green' text='Add'/>
+    <header className='header'>
+        <h1>{title}</h1>
+        <Button color={showAdd ? 'red' : 'green'} text={showAdd ? 'Close': 'Add'} onClick={onAdd}/>
     </header>
   )
 }
