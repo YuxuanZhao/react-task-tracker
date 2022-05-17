f5f76c42166818740d9478b3c84fa11947cc9e73
yuxuanzhao <785315305@qq.com>
2022-05-17 18:20:02 +0100

    save task

--- src/App.js
+++ src/App.js
@@ -34,7 +34,9 @@ const toggleReminder = (id) => {
 }
 
 const addTask = (task) => {
-  console.log(task)
+  const id = Math.floor(Math.random() * 10000) + 1
+  const newTask = { id, ...task }
+  setTasks([...tasks, newTask])
 }
 
   return (
